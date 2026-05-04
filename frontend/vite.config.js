24691994ddb70e0013b9f29f2e81ce91e1f3ba71
VedantBandre <vedantbandre33@gmail.com>
2026-05-04 17:09:00 +0200

    cards fin

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/login-attempt': 'http://localhost:8000',
+      '/events': 'http://localhost:8000',
+      '/suspicious': 'http://localhost:8000',
+      '/stats': 'http://localhost:8000',
+    }
+  }
 })
